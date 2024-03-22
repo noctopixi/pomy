ddadb5bf715a4dc1f4b6d31fab6306f6ba1423a4
Noctopixi <164736486+noctopixi@users.noreply.github.com>
2024-03-21 22:34:34 -0600

    Play chimes on cycle start
    Work and break cycles have different chimes each.
    Only Linux is supported at the moment.

--- pomy.py
+++ pomy.py
@@ -3,6 +3,9 @@
 from time import sleep
 from datetime import datetime
 from sys import argv
+import platform
+from subprocess import run
+from shutil import which
 
 minute_seconds = 60 if "--test" not in argv else 1
 cycle_number = 1
@@ -12,6 +15,24 @@ pomodoro_duration = 25 * minute_seconds
 short_break_duration = 5 * minute_seconds
 long_break_duration = 15 * minute_seconds
 
+operating_system = platform.system()
+sound_binary = None
+pomodoro_sfx = "sfx/pomodoro_sfx.wav"
+break_sfx = "sfx/break_sfx.wav"
+
+if not any(arg in argv for arg in ["--quiet", "-q"]):
+    # Check if a sound binary is present, default to aplay
+    if which("aplay"):
+        sound_binary = "aplay"
+    elif which("play"):
+        sound_binary = "play"
+
+
+def play_sfx(sound_effect):
+    if operating_system == "Linux":
+        run(["aplay", "-q", sound_effect])
+
+
 # Text Format
 BOLD = "\033[1m"
 RESET_FORMAT = "\033[0m"
@@ -20,15 +41,23 @@ RESET_FORMAT = "\033[0m"
 def set_cycle_type(cycle_number: int):
     # Long break every 8th cycle
     if cycle_number % 8 == 0:
-        return (long_break_duration, "Long break - You deserve it! (15m)")
+        is_work = False
+        return (long_break_duration, "Long break - You deserve it! (15m)", is_work)
     # Short breaks are always even
     elif cycle_number % 2 == 0:
-        return (short_break_duration, "Short break - Drink water and stretch! (5m)")
+        is_work = False
+        return (
+            short_break_duration,
+            "Short break - Drink water and stretch! (5m)",
+            is_work,
+        )
     # Pomodoro cycles are always odd
     elif cycle_number % 2 != 0:
-        return (pomodoro_duration, "Work time - Let's do this! (25m)")
+        is_work = True
+        return (pomodoro_duration, "Work time - Let's do this! (25m)", is_work)
     else:
-        return (pomodoro_duration, "Work time - Let's do this! (25m)")
+        is_work = True
+        return (pomodoro_duration, "Work time - Let's do this! (25m)", is_work)
 
 
 # Functions
@@ -56,7 +85,13 @@ while True:
         next_cycle = set_cycle_type(cycle_number)
         cycle_duration = next_cycle[0]
         cycle_msg = next_cycle[1]
+        cycle_is_work = next_cycle[2]
         show_progress(cycle_number, message=cycle_msg)
+        if sound_binary:
+            if cycle_is_work:
+                play_sfx(pomodoro_sfx)
+            else:
+                play_sfx(break_sfx)
         countdown(cycle_duration)
         cycle_number += 1
 
